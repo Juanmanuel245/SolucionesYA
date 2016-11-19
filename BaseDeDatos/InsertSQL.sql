fc51e163e9ea5cc413f82a90eb1487391553c8fa
Brian kuz <briankuz.l@gmail.com>
2016-11-19 19:17:51 -0300

    Se agrego insert de usuario

--- BaseDeDatos/InsertSQL.sql
+++ BaseDeDatos/InsertSQL.sql
@@ -1,3 +1,4 @@
+USE solucionesya
 INSERT INTO zona (nombre) values
 ("Capital Federal"), ("Ramos Mejia"), ("Moron"), ("Moreno");
 
@@ -12,7 +13,9 @@ INSERT INTO Especialidad (nombreEspecialidad) values
 
 INSERT INTO Usuario (apellido, email, logoEmpresa, nombre, nombreEmpresa, password, telefono, vecesContratado, balance, ID_GALERIA, ID_REPUTACION) values
 ("Caffi", "juanmanuelcaffi@gmail.com", "images/emp3.jpg", "Juan Manuel", "Caffi SA", "123456", "1159423002", 0, 0, 1, 3),
-("Escobar", "nicolase@gmail.com", "images/sinLogo.jpg", "Nicolas", "Browser SRL", "123456", "1159423002", 0, 0, 1, 3);
+("Escobar", "nicolase@gmail.com", "images/sinLogo.jpg", "Nicolas", "Browser SRL", "123456", "1159423002", 0, 0, 1, 3),
+("KUZ", "briankuz@gmail.com", "images/emp4.jpg", "Brian", "KUZ SRL", "123456", "1159423002", 0, 0, 1, 3);
+
 
 INSERT INTO Publicacion (contenido, cantidadContratos,  ID_ESPECIALIDAD, ID_USUARIO, ID_ZONA) values
 ("texto generado", 0, 1, 2, 1),
