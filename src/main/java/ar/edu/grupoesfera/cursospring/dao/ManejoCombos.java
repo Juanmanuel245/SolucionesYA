7e18ddbc51804fefe6aa48104ef211b07043521b
BrianKuz <briankuz.l@gmail.com>
2017-05-21 16:23:39 -0300

    se agrego logica de comando

--- src/main/java/ar/edu/grupoesfera/cursospring/dao/ManejoCombos.java
+++ src/main/java/ar/edu/grupoesfera/cursospring/dao/ManejoCombos.java
@@ -2,10 +2,13 @@ package ar.edu.grupoesfera.cursospring.dao;
 
 import java.util.List;
 
+
 import ar.edu.grupoesfera.cursospring.modelo.Especialidad;
 import ar.edu.grupoesfera.cursospring.modelo.Zona;
 
-public interface ManejoCombos {
+public interface ManejoCombos
+
+ {
 	
 	List<Zona> traerZonas();
 	List<Especialidad> traerEspecialidades();
